1b58104025fa1aa087ff6976ab482483ca7b3fcd
shronal <shronalduwal999@gmail.com>
2026-03-03 11:41:18 +0545

    Initial commit

---
+++ first.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");  // This line prints "Hello, World!" to the console.
+// How to run this code:
+// 1. Save this code in a file named "first.js".
+// 2. Create an HTML file (e.g., "index.html") and include this JavaScript file using a <script> tag:
+// But here you will find nothing because the code is only printing to the console, not to the webpage.
+// You can check the message in console by inspecting the webpage (index.html)
